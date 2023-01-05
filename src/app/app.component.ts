b3e4f81ace1f226e9a0385fc3e56c946fb651ac8
Eswar <eswar@levadata.com>
2023-01-05 19:50:35 +0530

    structural directive: ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,115 +20,41 @@ export interface Student{
 })
 export class AppComponent {
   
-// string
 
-// location: string | number | boolean | undefined | null ;
+x = 20;
+y = 30;
 
-location: any;
-
-userName = 'raju';
+c: number | undefined;
 
 
-updateMyLocation(){
-  this.location = "Hyd";
-  this.location = 10;
-  this.location = true;
-  this.location = undefined;
-  this.location = null;
-}
-
-
-fisrName = 'Naga';
-lastname = 'Kumar';
-
-
-
-
-//Number
-
-x = 123.123;
-
-y = '20';
-
-
-// boolean
-
-
-isStudent: boolean = true;
-isTeacher: boolean = false;
-
-
-
-// Array
-
-abc: Array<number> = [10,20,30];
-
-abc1: Array<string> = ['User 1', 'User 2', 'User 3'];
-
-abc2: Array<boolean> = [true, false, true];
-
-abc3: Array<number | string> = [10,20,'User1','User2']
-
-abc4: Array<number | string | boolean> = [10,20,'User1','User2', true]
-abc5: Array<any> = [10,20,'User1','User2', true]
-
-
-abc6: Array<Student> = [
-  {
-    name: 'Raju',
-    age: 10
-  },
-  {
-    name: 'Siva',
-    age: 20
-  },
-  {
-    name: 'Kumar',
-    age: 30
-  }
-]
-
-
-
-// Objects
-
-student: StudentObject = {
-  name: "Raju",
-  isStudent: false
-};
-
-
-
-
-
-
-
+boxStatus: boolean = true;
 
 constructor(){
 
-  this.student ={
-    name : "Siva",
-    isStudent: true
-  };
-
-  this.sum();
-  console.log(Number(this.fisrName) + this.lastname);
 }
 
 sum(){
-  console.log(Number(this.x) + Number(this.y))
+  console.log("Sum Calculation init")
+  this.c = this.x + this.y;
+}
+
+multi(){
+this.c = this.x * this.y;
+}
+
+sub(){
+  this.c = this.x - this.y;
+}
+mod(){
+  this.c = this.x % this.y;
+}
+
+showBox(){
+  this.boxStatus = true;
+}
+hideBox(){
+  this.boxStatus = false;
 }
 
 
-
-
-
-
-
-
-
-
-
-
- 
 }
