ce60f1ddbc9438ad5794eaf23276f740d4bc3cf2
Eswar <eswar@levadata.com>
2023-01-05 19:27:58 +0530

    Data types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,133 @@
 import { Component } from '@angular/core';
 
+
+export interface StudentObject{
+  name: string;
+  address?: string;
+  mobile?: string;
+  isStudent: boolean
+}
+
+export interface Student{
+  name: string;
+  age: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo-app';
+  
+// string
+
+// location: string | number | boolean | undefined | null ;
+
+location: any;
+
+userName = 'raju';
 
 
-  showMe ="show";
+updateMyLocation(){
+  this.location = "Hyd";
+  this.location = 10;
+  this.location = true;
+  this.location = undefined;
+  this.location = null;
+}
 
 
-  x = 10;
+fisrName = 'Naga';
+lastname = 'Kumar';
 
-  y= 5;
 
-  // Strings
 
-  // Number
 
-  // Boolen
+//Number
 
-  isStudent: boolean = true;
+x = 123.123;
 
-  // Object
+y = '20';
 
-  student = {
-    sNo: 123,
-    name: "Raju",
-    address: "HYd",
-    mobile: 123
+
+// boolean
+
+
+isStudent: boolean = true;
+isTeacher: boolean = false;
+
+
+
+// Array
+
+abc: Array<number> = [10,20,30];
+
+abc1: Array<string> = ['User 1', 'User 2', 'User 3'];
+
+abc2: Array<boolean> = [true, false, true];
+
+abc3: Array<number | string> = [10,20,'User1','User2']
+
+abc4: Array<number | string | boolean> = [10,20,'User1','User2', true]
+abc5: Array<any> = [10,20,'User1','User2', true]
+
+
+abc6: Array<Student> = [
+  {
+    name: 'Raju',
+    age: 10
+  },
+  {
+    name: 'Siva',
+    age: 20
+  },
+  {
+    name: 'Kumar',
+    age: 30
   }
+]
 
-  // Array
 
-  marks =[10,23,12,34,23,11];
+
+// Objects
+
+student: StudentObject = {
+  name: "Raju",
+  isStudent: false
+};
+
+
+
+
+
+
+
+
+constructor(){
+
+  this.student ={
+    name : "Siva",
+    isStudent: true
+  };
+
+  this.sum();
+  console.log(Number(this.fisrName) + this.lastname);
+}
+
+sum(){
+  console.log(Number(this.x) + Number(this.y))
+}
+
+
+
+
+
+
+
+
+
+
 
 
  
