38d3d20517bad6c5defe5ae44ed1fa7f75f16a6e
Eswar <eswar@levadata.com>
2023-01-17 20:27:06 +0530

    Task: Get Student Information based on button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,53 @@ import { ButtonEmitObject } from './shared/button/button.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+
+  studetnList: Array<any> = [
+    {
+      rollId: 1001,
+      name: 'Raju',
+      age: 12,
+      location: 'Hyd', 
+      class: 'IV',
+      section: "B",
+      mobile: '+91-987654321' 
+    },
+    {
+      rollId: 1002,
+      name: 'Venkat',
+      age: 11,
+      location: 'Vizag', 
+      class: 'V',
+      section: "A",
+      mobile: '+91-987624321' 
+    },
+    {
+      rollId: 1003,
+      name: 'Siva',
+      age: 13,
+      location: 'Del', 
+      class: 'VI',
+      section: "C",
+      mobile: '+91-987621321' 
+    }
+  ]
+
+  /*
+    Hyd - text-danger
+    Vizag - text-success
+    Del - text-Info
+   */
+
+  selectedStudentInformation: any = {
+    rollId: 1001,
+    name: 'Raju',
+    age: 12,
+    location: 'Hyd', // "Vizag" , "DEL" 
+    class: 'IV',
+    section: "B",
+    mobile: '+91-987654321' // optional
+  }
     constructor(){}
 
     buttonClick(event: ButtonEmitObject){
