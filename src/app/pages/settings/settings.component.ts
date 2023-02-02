7908deefff4e9f8b43a88ce3240209628e4fd1c5
Eswar <eswar@levadata.com>
2023-02-02 20:33:32 +0530

    Example FOr Form Builder

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -8,23 +8,33 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class SettingsComponent {
 
-  formGroupRef = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(5)]),
-    gender: new FormControl(''),
-    userType: new FormControl(''),
-    address: new FormGroup({
-      address1: new FormControl(''),
-      address2: new FormControl(''),
-      pincode: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  //   formGroupRef = new FormGroup({
+  //     email: new FormControl('', [Validators.required, Validators.email]),
+  //     password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+  //     gender: new FormControl(''),
+  //     userType: new FormControl(''),
+  //     address: new FormGroup({
+  //       address1: new FormControl(''),
+  //       address2: new FormControl(''),
+  //       pincode: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  //     })
+  //   })
+
+
+  formGroupRef = this.fb.group({
+    email: ['aba@abc.com', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+    gender: [''],
+    userType: [''],
+    address: this.fb.group({
+      address1: [''],
+      address2: [''],
+      pincode: ['', [Validators.required, Validators.minLength(6)]]
     })
   })
 
 
-
-  constructor(
-    private fb: FormBuilder
-  ) { }
+  constructor(private fb: FormBuilder) { }
 
 
   onUserLogin() {
